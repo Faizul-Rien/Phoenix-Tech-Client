1716b51284bb0fee78e1ace9e04a075b89857e3a
Faizul-Hassan-2015 <86608620+Faizul-Hassan-2015@users.noreply.github.com>
2022-05-23 18:03:56 +0600

    send purchase information to database

--- src/Pages/PurchaseParts/PurchaseParts.js
+++ src/Pages/PurchaseParts/PurchaseParts.js
@@ -1,9 +1,44 @@
 import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { useNavigate, useParams } from 'react-router-dom';
+import { toast } from 'react-toastify';
+import auth from '../../firebase.init';
 
 const PurchaseParts = () => {
+    const nevigate = useNavigate()
+    const [user, ] = useAuthState(auth);
     const {purchaseId} = useParams()
     const [parts,setParts] = useState({})
+
+    const handlePurchase = event => {
+        event.preventDefault();
+        nevigate('/dashboard');
+        
+    const purchase = {
+        purchaseId: parts._id,
+        parts: parts.name,
+        price : parts.price,
+        user: user.email,
+        userName: user.displayName,
+        // quantity : parts.event.target.quantity.value
+        
+    }
+    fetch('http://localhost:5000/purchase', {
+        method: 'POST',
+        headers: {
+            'content-type': 'application/json'
+        },
+        body: JSON.stringify(purchase)
+    })
+        .then(res => res.json())
+        .then(data => {
+            if(data.success){
+                toast('Purchase is set to Database')
+            }
+            
+        });
+}
+
     useEffect(()=>{
         fetch(`http://localhost:5000/part/${purchaseId}`)
         .then(res=> res.json())
@@ -12,7 +47,7 @@ const PurchaseParts = () => {
     return (
         <div class="card w-96 bg-blue-100 m-10 shadow-xl">
   <figure class="px-10 pt-10">
-    <img src={parts.img} alt="Shoes" class="rounded-xl" />
+    <img src={parts.img} alt="parts" class="rounded-xl" />
   </figure>
   <div class="card-body items-start text-left">
     <h2 class="card-title">{parts.name}</h2>
@@ -20,11 +55,11 @@ const PurchaseParts = () => {
     <p>Minumum Quantity : {parts.minquantity}</p>
     <p>Available Quantity : {parts.avaquantity}</p>
     <p>Description : {parts.description}</p>
-    <input type="text" placeholder="Name" class="input input-bordered input-accent w-full max-w-xs" />
-    <input type="email" placeholder="Email" class="input input-bordered input-accent w-full max-w-xs" />
-    <input type="text" placeholder="Quantity" class="input input-bordered input-accent w-28 max-w-xs" />
+    <input type="text" name='name' disabled value={user?.displayName || ""} placeholder="Name"  class="input input-bordered input-accent w-full max-w-xs" />
+    <input type="email" name='email' disabled value={user?.email || ''} placeholder="Email" class="input input-bordered input-accent w-full max-w-xs" />
+    <input type="text"  name='quantity' placeholder="Quantity" class="input input-bordered input-accent w-28 max-w-xs" />
     <div class="card-actions">
-      <button class="btn bg-green-400">BUY NOW</button>
+      <button onClick={handlePurchase} class="btn bg-green-400">BUY NOW</button>
     </div>
   </div>
 </div>
